5255ed232c61d83822b084de5a6f0579a4786eae
Taro Omiya <taro.omiya@omiyagames.com>
2020-02-11 23:06:14 -0500

    First attempt at integrating bulma.

--- footer.php
+++ footer.php
@@ -10,27 +10,28 @@
  */
 
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+<footer class="footer">
+	<div class="site-info content has-text-centered">
+		<p>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'game-dev-portfolio' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'game-dev-portfolio' ), 'WordPress' );
-				?>
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf( esc_html__( 'Powered by %s', 'game-dev-portfolio' ), 'WordPress' );
+			?>
 			</a>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'game-dev-portfolio' ), 'game-dev-portfolio', '<a href="https://taroomiya.com">Taro Omiya</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+			<a href="<?php echo esc_url( __( 'https://bulma.io/', 'game-dev-portfolio' ) ); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf( esc_html__( 'Styled by %s', 'game-dev-portfolio' ), 'Bulma' );
+			?>
+			</a>
+			<span class="sep"> | </span>
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			echo( esc_html__( 'Theme by Taro Omiya', 'game-dev-portfolio' ) );
+			?>
+		</p>
+		<?php wp_footer(); ?>
+	</div><!-- .site-info -->
+</footer>
--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,31 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+<nav class="column is-3">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'game-dev-portfolio' ); ?></a>
+	<div id="secondary" class="widget-area card menu">
+		<div class="card-image">
+			<figure class="site-branding image is-square">
+				<?php the_custom_logo(); ?>
+			</figure>
+		</div>
+		<div class="card-content">
+			<p class="title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</p>
+			<?php
+			$game_dev_portfolio_description = get_bloginfo( 'description', 'display' );
+			if ( $game_dev_portfolio_description || is_customize_preview() ) :
+			?>
+				<p class="site-description subtitle">
+					<?php echo $game_dev_portfolio_description; /* WPCS: xss ok. */ ?>
+				</p>
+			<?php
+			endif;
+			?>
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</div>
+	</div>
+</nav>
