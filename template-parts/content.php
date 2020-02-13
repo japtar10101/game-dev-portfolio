c2d4e9fa0d0c51b38ffcd3ea75dccdb4dc83ffaf
Taro Omiya <taro.omiya@omiyagames.com>
2020-02-13 00:59:54 -0500

    Updating the visuals of an individual post.

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,25 +9,24 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'content' ); ?>>
+	<?php game_dev_portfolio_post_thumbnail(); ?>
+	<?php
+	if ( is_singular() ) :
+		the_title( '<h1 class="title entry-title">', '</h1>' );
+	else :
+		the_title( '<h2 class="title entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+	endif;
 
-		if ( 'post' === get_post_type() ) :
+	if ( 'post' === get_post_type() ) :
+	?>
+		<div class="subtitle">
+			<?php
+			game_dev_portfolio_posted_on();
+			game_dev_portfolio_posted_by();
 			?>
-			<div class="entry-meta">
-				<?php
-				game_dev_portfolio_posted_on();
-				game_dev_portfolio_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+		</div><!-- .entry-meta -->
+	<?php endif; ?>
 
 	<?php game_dev_portfolio_post_thumbnail(); ?>
 
@@ -52,8 +51,4 @@
 		) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php game_dev_portfolio_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
