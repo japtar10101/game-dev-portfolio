9fa9282f3a920145ed6a5a244411106005662398
Taro Omiya <taro.omiya@omiyagames.com>
2020-02-13 18:13:41 -0500

    Updating archive page design to match with other sites.  Still needs formatting to make clear split between results.

--- archive.php
+++ archive.php
@@ -6,48 +6,52 @@
  *
  * @package Game_Dev_Portfolio
  */
-
-get_header();
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<?php get_header(); ?>
+<body <?php body_class(); ?>>
+	<section class="site section">
+		<div id="content" class="site-content container">
+				<!-- Start columns here -->
+				<div class="columns">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+					<!-- Left column (sidebar) here -->
+					<?php get_sidebar(); ?>
 
-		<?php if ( have_posts() ) : ?>
+					<!-- Right column (content) here -->
+					<main id="main" class="site-main content-area column">
+						<?php if ( have_posts() ) :
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+							the_archive_title( '<h1 class="title page-title">', '</h1>' );
+							the_archive_description( '<div class="subtitle archive-description">', '</div>' );
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+								/*
+								* Include the Post-Type-specific template for the content.
+								* If you want to override this in a child theme, then include a file
+								* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+								*/
+								get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+							endwhile;
 
-			the_posts_navigation();
+							the_posts_navigation();
 
-		else :
+						else :
 
-			get_template_part( 'template-parts/content', 'none' );
+							get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+						endif;
+						?>
+					</main><!-- #main -->
+				</div>
+			</div>
+		</div>
+	</section><!-- #primary -->
+	<?php get_footer(); ?>
+</body>
+</html>
