260d7b6f556696107ae416ca7d7cfe7caede10b3
Taro Omiya <japtar10101@duck.com>
2024-07-24 20:13:01 -0500

    Starting on converting the default footer to have the appearance as the old footer.

--- patterns/footer.php
+++ patterns/footer.php
@@ -1,124 +1,67 @@
 <?php
 /**
- * Title: Footer with colophon, 4 columns
- * Slug: twentytwentyfour/footer
+ * Title: Footer, 1 column
+ * Slug: game-dev-portfolio/footer
  * Categories: footer
  * Block Types: core/template-part/footer
- * Description: A footer section with a colophon and 4 columns.
+ * Description: A Bulma-styled footer section with 1 column.
  */
 ?>
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
-	<!-- wp:columns {"align":"wide"} -->
-	<div class="wp-block-columns alignwide">
-		<!-- wp:column {"width":"30%"} -->
-		<div class="wp-block-column" style="flex-basis:30%">
-			<!-- wp:group {"style":{"dimensions":{"minHeight":""},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
-			<div class="wp-block-group">
-				<!-- wp:site-logo {"width":20,"shouldSyncIcon":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
-
-				<!-- wp:site-title {"level":0,"fontSize":"medium"} /-->
-
-				<!-- wp:site-tagline {"fontSize":"small"} /-->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column {"width":"20%"} -->
-		<div class="wp-block-column" style="flex-basis:20%">
-		</div>
-		<!-- /wp:column -->
-
-		<!-- wp:column {"width":"50%"} -->
-		<div class="wp-block-column" style="flex-basis:50%">
-			<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-			<div class="wp-block-group">
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'About', 'twentytwentyfour' ); ?></h2>
-					<!-- /wp:heading -->
-
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
-
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'About', 'twentytwentyfour' ); ?>"} -->
-
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Team', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'History', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Careers', 'twentytwentyfour' ); ?>","url":"#"} /-->
-
-						<!-- /wp:navigation -->
-
-					</div>
-					<!-- /wp:group -->
-				</div>
-
-				<!-- /wp:group -->
-
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'Privacy', 'twentytwentyfour' ); ?></h2>
-					<!-- /wp:heading -->
-
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
-
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'Privacy', 'twentytwentyfour' ); ?>"} -->
-
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Privacy Policy', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Terms and Conditions', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Contact Us', 'twentytwentyfour' ); ?>","url":"#"} /-->
-
-						<!-- /wp:navigation -->
-
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:group -->
-
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'Social', 'twentytwentyfour' ); ?></h2>
-					<!-- /wp:heading -->
-
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
-
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'Social Media', 'twentytwentyfour' ); ?>"} -->
-
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Facebook', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Instagram', 'twentytwentyfour' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Twitter/X', 'twentytwentyfour' ); ?>","url":"#"} /-->
-
-						<!-- /wp:navigation -->
-
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:group -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"0"}}}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:0">
-		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast-2","fontSize":"small"} -->
-		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size">
+<div class="wp-block-group footer">
+	<!-- wp:group {"align":"wide"} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:paragraph {"align":"center","fontSize":"small","style":{"spacing":{"margin":{"top":"0"}}}} -->
+		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size has-text-centered" style="margin-block-start:0;">
+		<?php
+			$starting_year = '2008';
+			$author_name = 'Taro Omiya';
+			echo sprintf(
+				/* Translators: © 2008 - 2024 Taro Omiya */
+				esc_html__( '&copy; %1$s - %2$s %3$s', 'game-dev-portfolio' ),
+				$starting_year, date('Y'), $author_name
+			);
+			?>
+		</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"align":"center","fontSize":"small","style":{"spacing":{"margin":{"top":"0"}}}} -->
+		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size has-text-centered" style="margin-block-start:0;">
 		<?php
 			/* Translators: WordPress link. */
-			$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'twentytwentyfour' ) ) . '" rel="nofollow">WordPress</a>';
+			$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'game-dev-portfolio' ) ) . '" rel="nofollow">WordPress</a>';
 			echo sprintf(
 				/* Translators: Designed with WordPress */
-				esc_html__( 'Designed with %1$s', 'twentytwentyfour' ),
+				esc_html__( 'Designed with %1$s', 'game-dev-portfolio' ),
 				$wordpress_link
+			);
+			?>
+		</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"align":"center","fontSize":"small","style":{"spacing":{"margin":{"top":"0"}}}} -->
+		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size has-text-centered" style="margin-block-start:0;">
+		<?php
+			/* Translators: Game Dev Portfolio theme link. */
+			$theme_link = '<a href="' . esc_url( __( 'https://github.com/japtar10101/game-dev-portfolio', 'game-dev-portfolio' ) ) . '" rel="nofollow">Game Dev Portfolio</a>';
+			/* Translators: Taro Omiya link. */
+			$author_link = '<a href="' . esc_url( __( 'https://taroomiya.com', 'game-dev-portfolio' ) ) . '" rel="nofollow">Taro Omiya</a>';
+			echo sprintf(
+				/* Translators: Designed with WordPress */
+				esc_html__( 'Theme: %1$s by %2$s', 'game-dev-portfolio' ),
+				$theme_link, $author_link
+			);
+			?>
+		</p>
+		<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"align":"center","fontSize":"small","style":{"spacing":{"margin":{"top":"0"}}}} -->
+		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size has-text-centered" style="margin-block-start:0;">
+		<?php
+			/* Translators: Bulma link. */
+			$bulma_link = '<a href="' . esc_url( __( 'https://bulma.io', 'game-dev-portfolio' ) ) . '" rel="nofollow">Bulma</a>';
+			echo sprintf(
+				/* Translators: Styled by Bulma */
+				esc_html__( 'Styled by %1$s', 'game-dev-portfolio' ),
+				$bulma_link
 			);
 			?>
 		</p>
