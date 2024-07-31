101f87738a03da387a9ab389e2434f3bb3f92dd3
Taro Omiya <japtar10101@duck.com>
2024-07-30 22:40:19 -0500

    Adding hidden-nav.php for now.  Looking for a different option this time.

---
+++ patterns/hidden-nav.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Shared Nav
+ * Slug: game-dev-portfolio/hidden-nav
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:navigation {"overlayMenu":"never","className":"is-style-slide","layout":{"type":"flex","orientation":"vertical","className":"is-style-slide","justifyContent":"stretch"}} /-->
+
+	<!-- wp:social-links {"iconColor":"base-2","iconColorValue":"#ffffff","iconBackgroundColor":"contrast","iconBackgroundColorValue":"#111111","openInNewTab":true,"className":"is-style-default"} -->
+	<ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default">
+		<!-- wp:social-link {"url":"https://www.linkedin.com/in/taroomiya/","service":"linkedin"} /-->
+		<!-- wp:social-link {"url":"https://github.com/japtar10101/","service":"github"} /-->
+		<!-- wp:social-link {"url":"https://omiyagames.itch.io/","service":"chain"} /-->
+	</ul>
+	<!-- /wp:social-links -->
+
+	<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-outside","buttonUseIcon":true,"style":{"border":{"radius":"0.5rem"}}} /-->
+</div>
+<!-- /wp:group -->
